30fb1ed0be6a734729816a82b3e5cb01ffc4804b
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-29 08:31:07 +0100

    DT-877 Use latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,22 +24,22 @@ dependencies {
   implementation("io.springfox:springfox-bean-validators:2.9.2")
 
   implementation( "com.google.code.gson:gson:2.8.6")
-  implementation("com.google.guava:guava:28.2-jre")
-  implementation("com.nimbusds:nimbus-jose-jwt:8.11")
+  implementation("com.google.guava:guava:29.0-jre")
+  implementation("com.nimbusds:nimbus-jose-jwt:8.17.1")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.10.3")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.11.0")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework:spring-jms")
-  implementation( platform ("com.amazonaws:aws-java-sdk-bom:1.11.750"))
+  implementation( platform ("com.amazonaws:aws-java-sdk-bom:1.11.792"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:6.2.4")
-  runtimeOnly("org.postgresql:postgresql:42.2.10")
+  runtimeOnly("org.flywaydb:flyway-core:6.4.3")
+  runtimeOnly("org.postgresql:postgresql:42.2.12")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.26.3")
   testImplementation("org.testcontainers:localstack:1.13.0")
-  testImplementation("org.awaitility:awaitility-kotlin:4.0.2")
+  testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 }
