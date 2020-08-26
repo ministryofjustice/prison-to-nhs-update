b7f3bcd0fc416d9107638ef0ff973cba83f62652
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2020-08-26 10:41:58 +0100

    DT 1080: updated helm deployment to use liveness and readiness endpoints (#42)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisontonhs/services/health/HealthCheckIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisontonhs/services/health/HealthCheckIntegrationTest.kt
@@ -191,6 +191,22 @@ class HealthCheckIntegrationTest : IntegrationTest() {
 
   }
 
+  @Test
+  fun `Health liveness page is accessible`() {
+    webTestClient.get().uri("/health/liveness")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody().jsonPath("status").isEqualTo("UP")
+  }
+
+  @Test
+  fun `Health readiness page is accessible`() {
+    webTestClient.get().uri("/health/readiness")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody().jsonPath("status").isEqualTo("UP")
+  }
+
   private fun subPing(status: Int) {
     oauthMockServer.stubFor(get("/auth/health/ping").willReturn(aResponse()
         .withHeader("Content-Type", "application/json")
