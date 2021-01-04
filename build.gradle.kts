623acd58a8226ce13c1fbc5c02264f3c005df3ee
Peter Phillips <peter@greenthistle.co.uk>
2021-01-04 12:22:38 +0000

    DT-1452: 🔒 Suppress false positive for lang-tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.2"
   kotlin("plugin.spring") version "1.4.21"
   kotlin("plugin.jpa") version "1.4.21"
 }
@@ -22,17 +22,17 @@ dependencies {
   implementation("io.springfox:springfox-bean-validators:2.9.2")
 
   implementation("com.google.code.gson:gson:2.8.6")
-  implementation("com.google.guava:guava:30.0-jre")
-  implementation("com.nimbusds:nimbus-jose-jwt:9.1.5")
+  implementation("com.google.guava:guava:30.1-jre")
+  implementation("com.nimbusds:nimbus-jose-jwt:9.4")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.0")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.918"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.930"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.3.1")
+  runtimeOnly("org.flywaydb:flyway-core:7.3.2")
   runtimeOnly("org.postgresql:postgresql:42.2.18")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
